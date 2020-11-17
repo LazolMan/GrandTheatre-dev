ca8adf7fb37caa1fdff4f0751d55623550754912
Bogdan <lysyukbogdan1@gmail.com>
2020-11-17 12:34:00 +0200

    fixed main block, add header, sidebar, about block

--- js/script.js
+++ js/script.js
@@ -1,22 +1,11 @@
-var mySwiper = new Swiper(".swiper-container", {
-  // Optional parameters
+var slider = new Swiper(".swiper-container", {
   direction: "horizontal",
   loop: true,
   lazy: true,
+  spaceBetween: 30,
 
-  // If we need pagination
-  //   pagination: {
-  //     el: ".swiper-pagination",
-  //   },
-
-  // Navigation arrows
   navigation: {
     nextEl: ".main-slider__bttn-next",
     prevEl: ".main-slider__bttn-prev",
   },
-
-  // And if we need scrollbar
-  //   scrollbar: {
-  //     el: ".swiper-scrollbar",
-  //   },
 });
